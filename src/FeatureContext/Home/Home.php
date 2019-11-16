3da2add5d70534a302bcf2bba3af3b629e469a84
radood <radu.arsici@gmail.com>
2019-11-16 17:59:37 +0000

    removed some garbage

--- src/FeatureContext/Home/Home.php
+++ src/FeatureContext/Home/Home.php
@@ -10,7 +10,6 @@ use Components\Helper;
 
 class Home extends Helper
 {
-    protected $scenarioName = '';
 
     /**
      * @When User clicks on contact us button
@@ -22,17 +21,6 @@ class Home extends Helper
     {
         $this->waitForElementVisible($this->btn_contactUs);
         $this->click($this->btn_contactUs);
-    }
-
-    /**
-     * @Then Campaign menu should be visible to User
-     *
-     * @throws \RuntimeException
-     */
-
-    public function verify_campaign_menu_visible()
-    {
-        $this->assertElementVisible($this->mnu_campaigns);
     }
 
 }
